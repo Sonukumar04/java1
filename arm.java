cda2a91e5d02ec1bdf1cc68af33c5313d0f736f3
Sonu Kumar <sonu.r.k31@gmail.com>
2018-07-12 01:13:35 -0700

    assignment4

---
+++ arm.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class arm
+{
+    public static void main(String args[])
+    {
+        int n, number, num=0, rem;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter any Positive Number : ");
+        n = scan.nextInt();
+		
+        number = n;
+		
+        while(number != 0)
+        {
+            rem = number%10;
+            num = num + rem*rem*rem;
+            number = number/10;
+        }
+        if(num == n)
+        {
+            System.out.print("Armstrong Number");
+        }
+        else
+        {
+            System.out.print("Not an Armstrong Number");
+        }
+    }
+}
